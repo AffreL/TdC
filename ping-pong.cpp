bc57c07db44904990883263c9e4593eab5016eb0
Lucas Affre <102764979+AffreL@users.noreply.github.com>
2024-04-20 11:48:58 -0300

    Create ping-pong.cpp

---
+++ ping-pong.cpp
@@ -0,0 +1,38 @@
+const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 4;   //Pin digital 3 para el Echo del sensor
+const int Buzzer = 6; // 
+
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  pinMode(Buzzer, OUTPUT);
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+}
+
+void loop()
+{
+
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);          //Hacemos una pausa de 100ms
+
+  if(d <= 20){
+    digitalWrite(Buzzer, HIGH);
+  }
+  else{
+    digitalWrite(Buzzer, LOW);
+  }
+}
