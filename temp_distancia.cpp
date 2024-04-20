d249d7bbface5e86128306d144c1bdb0f391084c
Lucas Affre <102764979+AffreL@users.noreply.github.com>
2024-04-20 11:58:55 -0300

    Create temp_distancia.cpp

---
+++ temp_distancia.cpp
@@ -0,0 +1,87 @@
+// Incluimos librería
+#include <DHT.h>
+ 
+// Definimos el pin digital donde se conecta el sensor
+#define DHTPIN 8
+// Dependiendo del tipo de sensor
+#define DHTTYPE DHT11
+
+const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 4;   //Pin digital 3 para el Echo del sensor
+const int Buzzer = 6; // 
+
+ 
+// Inicializamos el sensor DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  pinMode(Buzzer, OUTPUT);
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+
+  // Comenzamos el sensor DHT
+  dht.begin();
+
+}
+
+void loop()
+{
+
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+
+  if(d <= 20){
+    digitalWrite(Buzzer, HIGH);
+  }
+  else{
+    digitalWrite(Buzzer, LOW);
+  }
+
+    // Leemos la humedad relativa
+  float h = dht.readHumidity();
+  // Leemos la temperatura en grados centígrados (por defecto)
+  float t_temp = dht.readTemperature();
+  // Leemos la temperatura en grados Fahreheit
+  float f = dht.readTemperature(true);
+ 
+  // Comprobamos si ha habido algún error en la lectura
+  if (isnan(h) || isnan(t_temp) || isnan(f)) {
+    Serial.println("Error obteniendo los datos del sensor DHT11");
+    return;
+  }
+ 
+  // Calcular el índice de calor en Fahreheit
+  float hif = dht.computeHeatIndex(f, h);
+  // Calcular el índice de calor en grados centígrados
+  float hic = dht.computeHeatIndex(t_temp, h, false);
+ 
+  Serial.print("Humedad: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatura: ");
+  Serial.print(t_temp);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Índice de calor: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+      // Esperamos 5 segundos entre medidas
+  delay(1000);
+}
