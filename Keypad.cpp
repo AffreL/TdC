c3c976f0e44f1aadf75090cc054efb8905f9a25b
Lucas Affre <102764979+AffreL@users.noreply.github.com>
2024-04-27 12:42:04 -0300

    Update Keypad.cpp

--- Keypad.cpp
+++ Keypad.cpp
@@ -1,3 +1,4 @@
+
 #include <Keypad.h>
  
 const byte rowsCount = 4;
@@ -14,15 +15,74 @@ const byte rowPins[rowsCount] = { 11, 10, 9, 8 };
 const byte columnPins[columsCount] = { 7, 6, 5, 4 };
  
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, columnPins, rowsCount, columsCount);
- 
+
+
+const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 3;   //Pin digital 3 para el Echo del sensor
+const int Buzzer = 1; // 
+
+
+
 void setup() {
-   Serial.begin(9600);
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  pinMode(Buzzer, OUTPUT);
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
 }
+
+void prueba(){
+    long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+  delay(100);          //Hacemos una pausa de 100ms
+
+  if(d <= 20){
+    digitalWrite(Buzzer, HIGH);
+  }
+  else{
+    digitalWrite(Buzzer, LOW);
+  }
+}
+
+String umbral;
+
+
+void loop()
+{
+  bool noTermino = true;
+
+
+  while(noTermino){
+    
+    char key = keypad.getKey();
+    if(key!=NO_KEY){
+      if(key!='#'){
+        Serial.println(key);
+        umbral += key;
+      }
+      else{
+        noTermino=false;
+        
+      }
+      
+    }
+     
+  }
  
-void loop() {
-   char key = keypad.getKey();
- 
-   if (key) {
-      Serial.println(key);
-   }
+
+  Serial.println(umbral);
+  umbral = "";
+
 }
